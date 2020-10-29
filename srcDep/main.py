cd0d97d7c3e4439766fc6c58455ad4d3ec68dd6c
Ryuzaki Lawliet <davide.lauterio@gmail.com>
2020-10-29 19:16:12 +0100

    it encrypts and decrypts 16 bytes blocks

---
+++ srcDep/main.py
@@ -0,0 +1,40 @@
+import src.utils as utils
+
+
+# initialize the S-Box and the inverted S-Box
+# initSBox() -> lookup table
+
+print("AES - 128")
+# generate a random string prompting the length
+inputPlain = utils.randomString(utils.promptStringLength())
+# inputPlain = utils.readInputsFromFile()
+# split the plain text in an array of 16Bytes each row
+inputPlainA = utils.split_blocks(inputPlain)
+
+
+# must get a key
+
+
+print("the input is: ", inputPlain)
+print("the splitted input is: ", inputPlainA)
+print("the length of each row is: ", [utils.utf8len(i) for i in inputPlainA])
+
+# initialize the state matrix from a row of the splitted plain text array
+# initState(plain16BytesString)
+
+# key transformation
+# keyT = doKeyTransformation(key)
+
+# key addition
+# doKeyWhitening(state, keyT)
+
+# compute Byte Substitution / Confusion
+# doConfusion(state, sBox)
+
+# compute the Diffusion Layer in separate functions
+# doShiftRows(state)
+# doMixColumns(state)
+
+# add Decryption stuff
+# todo
+
